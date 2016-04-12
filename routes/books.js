3bf0bccdfde686d254ffe45fbc9f55e486aacb15
austinsprague <sprague.ak@gmail.com>
2016-04-12 14:22:54 -0700

    some comments

--- routes/books.js
+++ routes/books.js
@@ -6,9 +6,17 @@ const knex = require('../db/knex');
 function Books() {
   return knex('books');
 }
+function Authors_Books() {
+  return knex('authors_books');
+}
 
 router.get('/', function(req, res, next) {
   Books().select().then(function (books) {
+    //inner join both tables
+
+    //array for unique id for book or author
+    //else append the array
+
     res.render('books/index', {
       allBooks: books
     });
